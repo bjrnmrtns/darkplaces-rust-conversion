9d98e92600c8d6f055cebf0418b5f716a6da6995
Bjorn Martens <bjorn@expeditious.nl>
2023-12-22 11:43:51 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -15,41 +15,45 @@ outputs = { self, nixpkgs, flake-utils, ... }:
         devShell = pkgs.mkShell {
           buildInputs = 
           [ 
-            pkgs.podman
-            pkgs.ripgrep
-            pkgs.zellij
-            pkgs.cargo
-            pkgs.cargo-watch
-            pkgs.rustc
-            pkgs.rustfmt
-            pkgs.clippy
-            pkgs.rust-analyzer
-            pkgs.helix
-            pkgs.neovim
-            pkgs.wget
-            pkgs.unzip
-	    pkgs.SDL2
-            pkgs.libjpeg
+#            pkgs.podman
+#            pkgs.ripgrep
+#            pkgs.zellij
+#            pkgs.cargo
+#            pkgs.cargo-watch
+#            pkgs.rustc
+#            pkgs.rustfmt
+#            pkgs.clippy
+#            pkgs.rust-analyzer
+#            pkgs.helix
+#            pkgs.neovim
+#            pkgs.wget
+#            pkgs.unzip
+#	    pkgs.SDL2
+#            pkgs.libjpeg
 
-	    pkgs.zlib
-	    pkgs.gcc
+#	    pkgs.zlib
+#	    pkgs.gcc
 
-	    pkgs.darwin.apple_sdk.frameworks.Carbon
-            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+#	    pkgs.darwin.apple_sdk.frameworks.Carbon
+       #     pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+       #     pkgs.darwin.apple_sdk.frameworks.CoreAudio
 
             # tool for converting from make to cmake and compiling in cmake build system
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.llvm
-	    pkgs.llvmPackages.libllvm
-	    pkgs.llvmPackages.libcxxClang
-	    pkgs.libiconv
+#            pkgs.cmake
+#            pkgs.ninja
+#	    pkgs.clang_11
+#	    pkgs.llvmPackages_11.libllvm
+	    #pkgs.llvmPackages.libcxxClang
+	    #pkgs.llvmPackages.stdenv
+#	    pkgs.llvmPackages_11.clangUseLLVM
+#	    pkgs.llvmPackages_11.clang-unwrapped
+#	    pkgs.libiconv
 
             # tools for converting to rust
-            pkgs.python3
-            pkgs.python3Packages.toml
-            pkgs.python3Packages.pyyaml
-            pkgs.python3Packages.cbor
+#            pkgs.python3
+#            pkgs.python3Packages.toml
+#            pkgs.python3Packages.pyyaml
+#            pkgs.python3Packages.cbor
           ];
       };
     });
